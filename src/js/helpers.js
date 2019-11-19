7908c6c9136f4ed9b0702554d33747824a2dbbbf
Martin Welte <martin.welte@towa.at>
2019-11-19 16:12:16 +0100

    es6 convert function

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -1,10 +1,14 @@
 export function convertHexColorToRgbString(hex, opacity) {
 	hex = hex.replace('#', '');
-	let r = parseInt(hex.substring(0, 2), 16);
-	let g = parseInt(hex.substring(2, 4), 16);
-	let b = parseInt(hex.substring(4, 6), 16);
 
-	return 'rgba(' + r + ',' + g + ',' + b + ',' + opacity + ')';
+	let colorChannels = Object.values({
+		r: parseInt(hex.substring(0, 2), 16),
+		g: parseInt(hex.substring(2, 4), 16),
+		b: parseInt(hex.substring(4, 6), 16),
+		a: opacity
+	}).join(',');
+
+	return `rgba(${colorChannels})`;
 }
 
 export function setCssClass(element, className, state) {
