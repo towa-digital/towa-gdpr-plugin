13f7c585630c5d6d12190bd2419c56c612603d36
Martin Welte <martin.welte@towa.at>
2020-01-14 17:39:10 +0100

    changed editor and code sniffing

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   '*.js': ['cross-env NODE_ENV=development node_modules/eslint/bin/eslint.js', 'git add'],
-  '*.php': ['composer run phpcbf', 'git add']
+  '*.php': ['composer phpcbf', 'git add']
 }
