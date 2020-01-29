efb9e254b40cc3731ac9373f6d15d80bd07e2480
Martin Welte <martin.welte@towa.at>
2020-01-29 11:51:03 +0100

    added sanitisation

--- src/Rest/Rest.php
+++ src/Rest/Rest.php
@@ -19,10 +19,20 @@ class Rest
 {
 	const TOWA_GDPR_REST_NAMESPACE = 'towa-gdpr/';
 
-	public function register_routes(){
+	/**
+	 * hook rest endpoint registration into rest register_api_init
+	 *
+	 * @return void
+	 */
+	public function register_routes(): void{
 		add_action('rest_api_init', array($this, 'register_rest_endpoint'));
 	}
 
+	/**
+	 * register rest endpoints of towa-gdpr-plugin
+	 *
+	 * @return void
+	 */
 	public function register_rest_endpoint():void {
 		register_rest_route( self::TOWA_GDPR_REST_NAMESPACE ,'consent/', array(
 			'methods' => WP_REST_Server::CREATABLE,
@@ -32,13 +42,15 @@ class Rest
 
 	/**
 	 * @param WP_REST_Request $request
+	 *
+	 * @return WP_REST_Response
 	 */
-	public function log_consent(WP_REST_Request $request){
-		// TODO: escaping
-		$hash = $request->get_param('hash');
-		$config = $request->get_param('config');
+	public function log_consent(WP_REST_Request $request): WP_REST_Response{
+		$hash = sanitize_key($request->get_param('hash'));
+		$config = json_encode($request->get_param('config'));
+		$url = sanitize_url($request->get_param('url'));
 		try{
-			(new Consent($config,$hash))->save();
+			(new Consent($config,$hash,$url))->save();
 			return new WP_REST_Response(null,200);
 		}
 		catch(exception $e){
