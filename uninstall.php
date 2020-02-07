79c12ccab8024ac6f72d59bbf0e03799e4f708f6
Martin Welte <martin.welte@towa.at>
2020-02-07 16:18:16 +0100

    uninstall cleanup

--- uninstall.php
+++ uninstall.php
@@ -1,5 +1,15 @@
 <?php
 
+/**
+ * Uninstall the Plugin
+ *
+ * This file is used to deinstall the towa-gdpr-cookie notice Plugin
+ *
+ * @author       Martin Welte
+ * @copyright    2019 Towa
+ * @license      GPL-2.0+
+ */
+
 if (!defined('WP_UNINSTALL_PLUGIN')) {
     exit;
 }
@@ -8,5 +18,6 @@ if (!defined('WP_UNINSTALL_PLUGIN')) {
  * Load plugin initialisation file.
  */
 require plugin_dir_path(__FILE__) . '/init.php';
-die('test');
+
+// uninstall complete Plugin
 \Towa\GdprPlugin\Plugin::uninstallPlugin();
